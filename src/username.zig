7f70b422ad45c3fe90e72ad20214e8fdb8c94b24
Eli <88557639+lishaduck@users.noreply.github.com>
2025-09-07 00:15:10 -0500

    fix: windows?
    Maybe? No way to test it, but it xcompiles now.

--- src/username.zig
+++ src/username.zig
@@ -6,28 +6,43 @@ const native_os = builtin.os.tag;
 
 username: []const u8,
 
-pub fn create(allocator: Allocator) !@This() {
+extern "advapi32" fn GetUserNameW(
+    lpBuffer: [*]u16,
+    pcbBuffer: *u32,
+) callconv(.winapi) std.os.windows.LSTATUS;
+
+pub const Error = error{
+    UserNotFound,
+    Unexpected,
+};
+
+pub fn create(allocator: Allocator) Error!@This() {
     if (native_os == .windows) {
         var buffer: [256]u16 = undefined;
         var size: u32 = 256;
 
-        const success = std.windows.GetUserNameW(&buffer[0], &size);
+        const success = GetUserNameW(&buffer, &size);
         if (success == 0) return error.UserNotFound;
 
-        return try std.unicode.utf16LeToUtf8Alloc(allocator, buffer[0..@intCast(size - 1)]);
-    } else {
-        const uid = std.c.getuid();
-        const pw = std.c.getpwuid(uid);
-        if (pw == null) return error.UserNotFound;
+        const name = std.unicode.utf16LeToUtf8Alloc(allocator, buffer[0..@intCast(size - 1)]) catch return error.Unexpected;
 
         return .{
-            .username = std.mem.span(pw.?.name).?,
+            .username = name,
         };
+    } else {
+        const uid = std.posix.getuid();
+        const pw = std.c.getpwuid(uid);
+
+        if (pw) |info| return .{
+            .username = std.mem.span(info.name orelse return error.UserNotFound),
+        };
+
+        return error.UserNotFound;
     }
 }
 
 pub fn deinit(self: @This(), allocator: Allocator) void {
     if (native_os == .windows) {
-        allocator.free(self);
+        allocator.free(self.username);
     }
 }
