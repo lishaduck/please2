9ff09eac6d80e9c96ddaff7a26907b0ac4400bf9
Eli <88557639+lishaduck@users.noreply.github.com>
2025-09-06 23:43:37 -0500

    chore: support -Dno-bin workflows

--- build.zig
+++ build.zig
@@ -8,6 +8,19 @@ pub fn build(b: *Build) void {
     });
     const name = "please2";
 
+    const no_bin = b.option(bool, "no-bin", "Do not emit binaries") orelse false;
+
+    // Global
+    const fmt_step = b.step("fmt", "Format all zig code");
+    const check_fmt_step = b.step("check-fmt", "Check formatting of all zig code");
+
+    const fmt_paths = .{ "src", "build.zig", "build.zig.zon" };
+    const fmt = b.addFmt(.{ .paths = &fmt_paths });
+    fmt_step.dependOn(&fmt.step);
+
+    const check_fmt = b.addFmt(.{ .paths = &fmt_paths, .check = true });
+    check_fmt_step.dependOn(&check_fmt.step);
+
     // Dependencies
     const zeit = b.dependency("zeit", .{});
     const zeit_mod = zeit.module("zeit");
@@ -24,15 +37,11 @@ pub fn build(b: *Build) void {
         .name = name,
         .root_module = exe_mod,
     });
-    b.installArtifact(exe);
-
-    // Check
-    const check = b.addExecutable(.{
-        .name = name,
-        .root_module = exe_mod,
-    });
-    const check_step = b.step("check", "Check the app compiles");
-    check_step.dependOn(&check.step);
+    if (no_bin) {
+        b.getInstallStep().dependOn(&exe.step);
+    } else {
+        b.installArtifact(exe);
+    }
 
     // Run
     const run_cmd = b.addRunArtifact(exe);
@@ -42,13 +51,4 @@ pub fn build(b: *Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Test
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
